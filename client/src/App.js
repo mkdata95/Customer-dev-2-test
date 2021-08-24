748ed10fb99d15702d8b73082ebee586ea003f04
mkdata95 <mkdata95@naver.com>
2021-08-24 17:51:42 +0900

    api 변경

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,12 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto",
   },
   table: {
@@ -21,33 +21,23 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "홍길동",
-    birthday: "921207",
-    gender: "남자",
-    job: "학생",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "나사랑",
-    birthday: "321207",
-    gender: "여자",
-    job: "학생",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "최민수",
-    birthday: "651103",
-    gender: "남자",
-    job: "배우",
-  },
-];
 class App extends Component {
+  state = {
+    customers: "",
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -63,21 +53,22 @@ class App extends Component {
               <TableCell>직업</TableCell>
             </TableRow>
           </TableHead>
-
           <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
+            {this.state.customers
+              ? this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -3,11 +3,36 @@ const bodyParser = require("body-Parser");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send("안녕");
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "홍길동",
+      birthday: "921207",
+      gender: "남자",
+      job: "학생",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "나사랑",
+      birthday: "321207",
+      gender: "여자",
+      job: "학생",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "최민수",
+      birthday: "651103",
+      gender: "남자",
+      job: "배우",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`${port} 포트로 작동 중입니다. `));
